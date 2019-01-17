29be038f5fd4c45cf1f5e0ceab8171a7d5f59a21
HassanGamal <hassangamal358@gmail.com>
2019-01-17 19:32:24 +0200

    Notification Design.

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.css"));
         }
     }
 }
